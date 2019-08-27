02afe721317263114f33bcaeaf82adec24112a91
Aaron Sickler <sicklera517@gmail.com>
2019-08-27 03:24:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -18,6 +18,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
   return array
 }
 
-function accessElementInArray() {
+function accessElementInArray(array) {
   console.log (array[n])
 }
