55e8c2af59cd1eb82b9f8e1faad518232792d819
Aaron Sickler <sicklera517@gmail.com>
2019-08-27 03:04:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,3 @@
-var cholcolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray() {}
 function destructivelyAddElementToBeginningOfArray
