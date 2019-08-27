a4db0202f35138b7f628ee18b486b46f37cc77ba
Aaron Sickler <sicklera517@gmail.com>
2019-08-27 03:33:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,7 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
 
-function destructivelyRemoveElementFromEndOfArray(array){
+function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop(element)
   return array
 }
